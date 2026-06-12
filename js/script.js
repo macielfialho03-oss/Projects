18dcdd21dec2336360a239681196ba515d4419f2
macielfialho03-oss <macielfialho03@gmail.com>
2026-06-12 19:29:31 -0400

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".form-cadastro").forEach(function (formulario) {
+  formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    alert("Cadastro realizado com sucesso!");
+    formulario.reset();
+  });
+});
